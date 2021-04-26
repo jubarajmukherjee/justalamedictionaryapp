3337e60cd292f3c006077f82d41891c90a2cd73c
Jubaraj Mukherjee <59380734+jubarajmukherjee@users.noreply.github.com>
2021-04-26 20:55:34 +0530

    Add files via upload

---
+++ Dictionary/README.txt
@@ -0,0 +1,25 @@
+Overview:
+I created this python applicaion to make a user-friendly dictionary for daily use and referncing.
+I followed a tutorial to make this.
+
+How to use:
+>Run the app in the command line
+>A prompt will appear asking you for the word you need the definition of
+>Input the word and press ENTER
+	>If the entered word is wrong will prompt a similar word
+	>Will ask if it is applicable (Y/N)
+	>Enter the choice
+>Will output the desired definition
+
+Features:
+This program is semi-smart. It will prompt a similar word when an error is made by the user while trying to input a name. 
+Hence, not just lame dictionary app.
+
+What I learnt from it?
+>In the 'difflib' module the 'get_close_matches' is a pretty powerful yet simple code. Might have many other uses outside the dictionary for getting close matches.
+
+
+Reach me 
+email> jubarajmukherjee@protonmai.com/mailtojubaraj@gmail.com
+linkedin>https://www.linkedin.com/in/jubarajmukherjee/
+twitter>@jubarajmukherje
--- Dictionary/dictionaryapp.py
+++ Dictionary/dictionaryapp.py
@@ -8,11 +8,11 @@ def translate(word):
 	word = word.lower() 	#converted to lowercase to remove case-sensitive error
 	if word in data:		#when word is present it's true, so checks for presence of word 
 		return data[word]	
-	elif len(get_close_matches(word,data.keys(),n=1,cutoff=0.8))>0:
+	elif len(get_close_matches(word,data.keys(),n=1,cutoff=0.8))>0:	#checks if there are any close matches to the word
 		yn = input("Did you mean %s? Press Y for Yes and N for No: " % get_close_matches(word,data.keys(),n=3,cutoff=0.8)[0])
-		yn = yn.lower()
+		yn = yn.lower() #converts input to lowercase to avoid case sentivity issues/errors
 		if yn =="y":
-			return data[get_close_matches(word,data.keys(),n=3,cutoff=0.8)[0]] 
+			return data[get_close_matches(word,data.keys(),n=3,cutoff=0.8)[0]] #returns the closest match to the word
 		elif yn =="n":
 			return "The word is not available in the dictionary"
 		else:
@@ -22,14 +22,14 @@ def translate(word):
 
 
 
-word=input("Enter the word: ")		#user input
+word=input("Enter the word: ")	#user input
 
 output = translate(word)
 
-if type(output) == list:
+if type(output) == list: #checks if the definition is returned from json as a list
 	for item in output:
-		print(item)
+		print(item) # helps to print the definition from json file one by one instead of a list, making it more readable
 else:
-	print(output)		
+	print(output)	# prints all the other cases
 
 
